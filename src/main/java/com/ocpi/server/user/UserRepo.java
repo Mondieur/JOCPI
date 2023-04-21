13e9ae5446b78b565c1988b9812c5b83c513f29b
Mondieur <garamoundier.developer@protonmail.com>
2023-04-20 23:10:05 -0300

    modified: UserRepo.java

--- src/main/java/com/ocpi/server/user/UserRepo.java
+++ src/main/java/com/ocpi/server/user/UserRepo.java
@@ -3,13 +3,12 @@ package com.ocpi.server.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, UUID> {
-
     User findById(int id);
     void deleteById(int id);
-    
-    // List<User> findAllByName();
+    List<User> findAllByName();
 }
