d46baa692bb0dbdcb42d8a5c365bd3acb486812a
Mondieur <garamoundier.developer@protonmail.com>
2023-04-22 02:24:43 -0300

    feat: add CourseRepo.java

--- src/main/java/com/ocpi/server/course/CourseRepo.java
+++ src/main/java/com/ocpi/server/course/CourseRepo.java
@@ -1,4 +1,10 @@
 package com.ocpi.server.course;
 
-public interface CourseRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepo extends JpaRepository<Course, Integer> {
+    Course findById(int id);
+    void deleteById(int id);
 }
