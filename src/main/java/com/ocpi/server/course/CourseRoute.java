c31cd2c61fab18969d3eccee28d34098ae9746a7
Mondieur <garamoundier.developer@protonmail.com>
2023-04-23 00:31:31 -0300

    (#33) add: ResponseEntity return type

--- src/main/java/com/ocpi/server/course/CourseRoute.java
+++ src/main/java/com/ocpi/server/course/CourseRoute.java
@@ -1,41 +1,42 @@
 package com.ocpi.server.course;
 
-import com.ocpi.server.Sfew;
-import com.ocpi.server.user.User;
+
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/course")
+@RequestMapping("/api/courses")
 @AllArgsConstructor
 @CrossOrigin(origins = "http://localhost:9090")
 public class CourseRoute {
 
     private final CourseService service;
 
-    @PostMapping("/save")
-    public void save(@RequestBody Course obj) {
-        service.save(obj);
+    @PostMapping
+    public ResponseEntity save(@Validated @RequestBody Object obj) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(obj);
     }
 
-    @GetMapping("/find/{id}")
+    @GetMapping("/{id}")
     public Object find(@PathVariable int id) {
         return service.find(id);
     }
 
-    @PutMapping("/edit")
+    @PutMapping("/{id}")
     public String edit(@RequestBody Course obj) {
         return service.edit(obj);
     }
 
-    @DeleteMapping("/wipe/{id}")
-    public void wipe(@PathVariable int id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity wipe(@PathVariable int id) {
         service.wipe(id);
-        System.out.println("deleted" + id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping(name = "", value = {}, path = "/all", params = {}, headers = {}, consumes = {}, produces = {})
@@ -45,7 +46,7 @@ public class CourseRoute {
         headers.set("Cache-Control", "max-age=3600");
 
         System.out.println("courses (none):" + courses);
-        System.out.println("headers (attr):" + headers);
+        System.out.println("headers (list):" + headers);
 
         return ResponseEntity.ok().headers(headers).body(courses);
     }
