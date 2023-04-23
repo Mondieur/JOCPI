501258a681104bb312b251f388bfa2db1cda1370
Mondieur <garamoundier.developer@protonmail.com>
2023-04-22 23:53:30 -0300

    (#29) update: findAll method

--- src/main/java/com/ocpi/server/course/CourseRepo.java
+++ src/main/java/com/ocpi/server/course/CourseRepo.java
@@ -3,8 +3,11 @@ package com.ocpi.server.course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepo extends JpaRepository<Course, Integer> {
     Course findById(int id);
+    List<Course> findAll();
     void deleteById(int id);
 }
